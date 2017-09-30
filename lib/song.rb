afd14c5d1ca261b4ac6f3d24e84957beee02cb0b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-30 02:16:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
   def self.all
     @@all
   end
@@ -10,4 +11,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new()
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(title)
+    @@all.each do |song|
+      if song.name == title
+        return song
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(title)
+    
+    if self.find_by_name(title) == nil
+       self.create_by_name(title)
+    else
+      self.find_by_name(title)
+    end
+  end
 end
